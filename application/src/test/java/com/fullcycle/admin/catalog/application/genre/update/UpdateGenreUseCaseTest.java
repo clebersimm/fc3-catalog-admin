8234d641a605ac0ca7dc4afa4ce9911e1f6bc28a
Cleber Simm <clebersimm@gmail.com>
2024-05-14 21:01:31 -0300

    feat: genre
    update genre, not working

--- application/src/test/java/com/fullcycle/admin/catalog/application/genre/update/UpdateGenreUseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalog/application/genre/update/UpdateGenreUseCaseTest.java
@@ -1,8 +1,8 @@
 package com.fullcycle.admin.catalog.application.genre.update;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Objects;
@@ -15,7 +15,6 @@ import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.hamcrest.MockitoHamcrest;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.fullcycle.admin.catalog.domain.category.CategoryGateway;
@@ -50,6 +49,68 @@ public class UpdateGenreUseCaseTest {
                 Assertions.assertNotNull(actualOutput);
                 Assertions.assertEquals(expectedId.getValue(), actualOutput.id());
                 Mockito.verify(genreGateway, times(1)).findById(expectedId);
+                Mockito.verify(genreGateway, times(1))
+                                .update(Mockito.argThat(aUpdatedGenre -> Objects.equals(expectedName,
+                                                aUpdatedGenre.getId())
+                                                && Objects.equals(expectedName, aUpdatedGenre.getName())
+                                                && Objects.equals(expectedIsActive, aUpdatedGenre.isActive())
+                                                && Objects.equals(expectedCategories, aUpdatedGenre.getCategories())
+                                                && Objects.equals(aGenre.getCreatedAt(), aUpdatedGenre.getCreatedAt())
+                                                && aGenre.getUpdatedAt().isBefore(aUpdatedGenre.getUpdatedAt())
+                                                && Objects.isNull(aUpdatedGenre.getDeletedAt())));
+        }
+
+        @Test
+        public void givenAValidCommandWithCategories_whenCallsCreateUpdateGenre_shouldReturnGenreId() {
+                // given
+                final var aGenre = Genre.newGenre("acao", true);
+                final var expectedId = aGenre.getId();
+                final var expectedName = "Ação";
+                final var expectedIsActive = true;
+                final var expectedCategories = List.<CategoryID>of(CategoryID.from("123"), CategoryID.from("456"));
+                final var aCommand = UpdateGenreCommand.with(expectedId.getValue(), expectedName, expectedIsActive,
+                                asString(expectedCategories));
+                Mockito.when(genreGateway.findById(any())).thenReturn(Optional.of(Genre.with(aGenre)));
+                Mockito.when(categoryGateway.existsById(any())).thenReturn(expectedCategories);
+                Mockito.when(genreGateway.update(any())).thenAnswer(AdditionalAnswers.returnsFirstArg());
+                // when
+                final var actualOutput = useCase.execute(aCommand);
+                // then
+                Assertions.assertNotNull(actualOutput);
+                Assertions.assertEquals(expectedId.getValue(), actualOutput.id());
+                Mockito.verify(genreGateway, times(1)).findById(expectedId);
+                Mockito.verify(categoryGateway, times(1)).existsById(eq(expectedCategories));
+                Mockito.verify(genreGateway, times(1))
+                                .update(Mockito.argThat(aUpdatedGenre -> Objects.equals(expectedName,
+                                                aUpdatedGenre.getId())
+                                                && Objects.equals(expectedName, aUpdatedGenre.getName())
+                                                && Objects.equals(expectedIsActive, aUpdatedGenre.isActive())
+                                                && Objects.equals(expectedCategories, aUpdatedGenre.getCategories())
+                                                && Objects.equals(aGenre.getCreatedAt(), aUpdatedGenre.getCreatedAt())
+                                                && aGenre.getUpdatedAt().isBefore(aUpdatedGenre.getUpdatedAt())
+                                                && Objects.isNull(aUpdatedGenre.getDeletedAt())));
+        }
+
+        @Test
+        public void givenAInvalidName_whenCallsCreateUpdateGenre_shouldReturnNotificationException() {
+                // given
+                final var aGenre = Genre.newGenre("acao", true);
+                final var expectedId = aGenre.getId();
+                final var expectedName = "Ação";
+                final var expectedIsActive = true;
+                final var expectedCategories = List.<CategoryID>of(CategoryID.from("123"), CategoryID.from("456"));
+                final var aCommand = UpdateGenreCommand.with(expectedId.getValue(), expectedName, expectedIsActive,
+                                asString(expectedCategories));
+                Mockito.when(genreGateway.findById(any())).thenReturn(Optional.of(Genre.with(aGenre)));
+                Mockito.when(categoryGateway.existsById(any())).thenReturn(expectedCategories);
+                Mockito.when(genreGateway.update(any())).thenAnswer(AdditionalAnswers.returnsFirstArg());
+                // when
+                final var actualOutput = useCase.execute(aCommand);
+                // then
+                Assertions.assertNotNull(actualOutput);
+                Assertions.assertEquals(expectedId.getValue(), actualOutput.id());
+                Mockito.verify(genreGateway, times(1)).findById(expectedId);
+                Mockito.verify(categoryGateway, times(1)).existsById(eq(expectedCategories));
                 Mockito.verify(genreGateway, times(1))
                                 .update(Mockito.argThat(aUpdatedGenre -> Objects.equals(expectedName,
                                                 aUpdatedGenre.getId())
